2e1ccbda7ebc8f91c341d554ef9b4099800c6888
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-12-12 09:11:27 +0530

    - added routing for app - installed react router dom using cmd npm install -D

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const  Login = () => {
+  return (
+    <div>
+        <Header/>
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/274d310a-9543-4b32-87f3-147b372abc00/web/IN-en-20251201-TRIFECTA-perspective_baf6d3bc-eece-4a63-bcbb-e0a2f5d9d9ec_large.jpg' alt='background'/>
+
+    </div>
+  )
+}
+
+export default Login
