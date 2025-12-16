698ee686946590bb38295661f9de6e4bd55efd49
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-12-16 10:05:40 +0530

    added constant for user avatar and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO, USER_AVATAR } from "../utils/constant";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -43,7 +44,7 @@ const Header = () => {
       <img
         className="w-44"
         alt="logo"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+        src={LOGO}
       />
 
       {user && (
@@ -51,7 +52,7 @@ const Header = () => {
           <img
             className="w-10 h-10 rounded"
             alt="usericon"
-            src="https://occ-0-3216-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229"
+            src={USER_AVATAR}
           />
           <button
             onClick={handleSignOut}
