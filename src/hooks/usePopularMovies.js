b2d0b56f2650b862ff2ebf845ee08acb60d8b341
Arjun <khadearjun@gmail.com>
2025-12-21 11:17:14 +0530

    NetflixGPT -created hook useMoviewTrailer and usePopular movies.

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { API_Options } from "../utils/constant";
+import { addPopularMovie } from "../utils/movieSlice";
+
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    popularMovies();
+  }, []);
+
+  const popularMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      API_Options
+    );
+    const json = await data.json();
+    console.log(json.results);
+    dispatch(addPopularMovie(json.results));
+  };
+};
+
+export default usePopularMovies;
