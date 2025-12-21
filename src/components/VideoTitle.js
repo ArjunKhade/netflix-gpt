e970de36958715444d59286a02b174d910f39abf
Arjun <khadearjun@gmail.com>
2025-12-21 11:20:41 +0530

    NetflixGPT -updated video title component. -code cleanup.

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className='pt-36 px-12' >
+    <div className='pt-[20%] px-24 absolute text-white bg-gradient-to-r from-black w-screen aspect-video' >
        <h1 className='text-6xl font-bold'>{title}</h1>
        <p className='py-6 text-lg w-1/3'>{overview}</p>
        <div className=''>
-          <button className='bg-gray-500 bg-opacity-50 rounded-lg py-4 px-12 text-xl text-white'>▶ Play</button>
-          <button className='bg-gray-500 mx-2 bg-opacity-50 rounded-lg py-4 px-12 text-xl text-white'>More Info</button>
+          <button className='bg-white hover:bg-opacity-80 rounded-lg py-4 px-12 text-xl text-black'> Play</button>
+          <button className='bg-gray-500 mx-2 hover:bg-opacity-80 bg-opacity-50 rounded-lg py-4 px-12 text-xl text-white'>More Info</button>
        </div>
     </div>
   )
