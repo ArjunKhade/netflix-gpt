6d9f06f9711d2856610f321faa3ce15799af005e
Arjun <khadearjun@gmail.com>
2025-12-25 13:07:16 +0530

    implement toggle gpt search

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -4,16 +4,26 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
+import GptSearch from "./GptSearch";
+import { useSelector } from "react-redux";
 
 const Browse = () => {
   useNowPlayingMovies();
   usePopularMovies();
 
+  const showGPTSearch = useSelector((store) => store.gpt?.showGptSearch);
+
   return (
     <div>
       <Header />
-      <MainContainer></MainContainer>
-      <SecondaryContainer></SecondaryContainer>
+      {showGPTSearch ? (
+        <GptSearch></GptSearch>
+      ) : (
+        <>
+          <MainContainer></MainContainer>
+          <SecondaryContainer></SecondaryContainer>
+        </>
+      )}
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,19 @@ import { auth } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO, USER_AVATAR } from "../utils/constant";
+import { LOGO, SUPPORTED_LANGUAGE, USER_AVATAR } from "../utils/constant";
+import { toggleGptSearch } from "../utils/gptSlice";
+import { changeLangugae } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
+  const showGPTSearch = useSelector((store) => store.gpt.showGptSearch);
+  const handleGptSearch = () => {
+    //handle on GPT search button clicked
+    dispatch(toggleGptSearch());
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -39,21 +46,32 @@ const Header = () => {
         // An error happened.
       });
   };
+
+const handleLangChange = (e) => {
+   dispatch(changeLangugae(e?.target?.value));
+}
+
   return (
     <div className=" z-10 flex justify-between absolute w-screen px-8 py-2 bg-gradient-to-b from-black">
-      <img
-        className="w-44"
-        alt="logo"
-        src={LOGO}
-      />
+      <img className="w-44" alt="logo" src={LOGO} />
 
-      {user && (
+      {true && (
         <div className="flex items-center">
-          <img
-            className="w-10 h-10 rounded"
-            alt="usericon"
-            src={USER_AVATAR}
-          />
+          { showGPTSearch &&
+            <select className="p-2 m-2 bg-gray-900 text-white" onChange={handleLangChange}> 
+            {SUPPORTED_LANGUAGE.map((lang) => (
+              <option key={lang.value} value={lang.Value}>{lang.Label}</option>
+            ))}
+            </select>
+          }
+
+          <button
+            onClick={handleGptSearch}
+            className="text-white p-2 mr-2 rounded bg-purple-800"
+          >
+            {showGPTSearch? "Home Page" : "GPT Search"}
+          </button>
+          <img className="w-10 h-10 rounded" alt="usericon" src={USER_AVATAR} />
           <button
             onClick={handleSignOut}
             className="ml-4 px-4 py-2 bg-red-600 text-white font-semibold rounded hover:bg-red-700 transition duration-200 cursor-pointer"
