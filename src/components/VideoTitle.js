337bddb58b36d1ddf60a0f3af822ab40cf656cb0
Arjun <khadearjun@gmail.com>
2025-12-28 11:31:16 +0530

    corrected height of video title

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const VideoTitle = ({title, overview}) => {
   return (
-    <div className='pt-[20%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black w-screen aspect-video' >
+    <div className='md:pt-[20%] pt-[43%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black w-screen aspect-video' >
        <h1 className='text-2xl md:text-6xl font-bold'>{title}</h1>
        <p className='py-6 text-lg w-1/3 hidden md:inline-block'>{overview}</p>
        <div className='mt-2 md:mt-0'>
